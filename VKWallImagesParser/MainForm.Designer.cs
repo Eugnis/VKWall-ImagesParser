ed9e613d6cac68386b0d3422e7651ea104bcb832
Eugene <Eugnis@users.noreply.github.com>
2016-08-23 22:11:44 +0300

    Add files via upload

---
+++ VKWallImagesParser/MainForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace VKWallImagesParser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBtn = new System.Windows.Forms.Button();
+            this.groupNameTxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cntNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.parseBtn = new System.Windows.Forms.Button();
+            this.parsedCntLabel = new System.Windows.Forms.Label();
+            this.downloadImgBtn = new System.Windows.Forms.Button();
+            this.downloadedCntLabel = new System.Windows.Forms.Label();
+            this.parsedInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cntNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.Location = new System.Drawing.Point(138, 70);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(67, 42);
+            this.checkBtn.TabIndex = 0;
+            this.checkBtn.Text = "Старт";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            this.checkBtn.Click += new System.EventHandler(this.checkBtn_Click);
+            // 
+            // groupNameTxtBox
+            // 
+            this.groupNameTxtBox.Location = new System.Drawing.Point(27, 78);
+            this.groupNameTxtBox.Name = "groupNameTxtBox";
+            this.groupNameTxtBox.Size = new System.Drawing.Size(105, 26);
+            this.groupNameTxtBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID/страница стены";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Кол-во постов: ";
+            // 
+            // cntNumericUpDown
+            // 
+            this.cntNumericUpDown.Location = new System.Drawing.Point(29, 158);
+            this.cntNumericUpDown.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.cntNumericUpDown.Name = "cntNumericUpDown";
+            this.cntNumericUpDown.Size = new System.Drawing.Size(120, 26);
+            this.cntNumericUpDown.TabIndex = 6;
+            // 
+            // parseBtn
+            // 
+            this.parseBtn.Location = new System.Drawing.Point(27, 206);
+            this.parseBtn.Name = "parseBtn";
+            this.parseBtn.Size = new System.Drawing.Size(178, 37);
+            this.parseBtn.TabIndex = 7;
+            this.parseBtn.Text = "Парсить";
+            this.parseBtn.UseVisualStyleBackColor = true;
+            this.parseBtn.Click += new System.EventHandler(this.parseBtn_Click);
+            // 
+            // parsedCntLabel
+            // 
+            this.parsedCntLabel.AutoSize = true;
+            this.parsedCntLabel.Location = new System.Drawing.Point(249, 81);
+            this.parsedCntLabel.Name = "parsedCntLabel";
+            this.parsedCntLabel.Size = new System.Drawing.Size(95, 20);
+            this.parsedCntLabel.TabIndex = 8;
+            this.parsedCntLabel.Text = "Спаршено: ";
+            // 
+            // downloadImgBtn
+            // 
+            this.downloadImgBtn.Enabled = false;
+            this.downloadImgBtn.Location = new System.Drawing.Point(253, 206);
+            this.downloadImgBtn.Name = "downloadImgBtn";
+            this.downloadImgBtn.Size = new System.Drawing.Size(178, 37);
+            this.downloadImgBtn.TabIndex = 9;
+            this.downloadImgBtn.Text = "Скачать";
+            this.downloadImgBtn.UseVisualStyleBackColor = true;
+            this.downloadImgBtn.Click += new System.EventHandler(this.downloadImgBtn_Click);
+            // 
+            // downloadedCntLabel
+            // 
+            this.downloadedCntLabel.AutoSize = true;
+            this.downloadedCntLabel.Location = new System.Drawing.Point(249, 163);
+            this.downloadedCntLabel.Name = "downloadedCntLabel";
+            this.downloadedCntLabel.Size = new System.Drawing.Size(0, 20);
+            this.downloadedCntLabel.TabIndex = 10;
+            // 
+            // parsedInfo
+            // 
+            this.parsedInfo.AutoSize = true;
+            this.parsedInfo.Location = new System.Drawing.Point(249, 122);
+            this.parsedInfo.Name = "parsedInfo";
+            this.parsedInfo.Size = new System.Drawing.Size(0, 20);
+            this.parsedInfo.TabIndex = 11;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 267);
+            this.Controls.Add(this.parsedInfo);
+            this.Controls.Add(this.downloadedCntLabel);
+            this.Controls.Add(this.downloadImgBtn);
+            this.Controls.Add(this.parsedCntLabel);
+            this.Controls.Add(this.parseBtn);
+            this.Controls.Add(this.cntNumericUpDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupNameTxtBox);
+            this.Controls.Add(this.checkBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "VKWall Images Parser";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cntNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkBtn;
+        private System.Windows.Forms.TextBox groupNameTxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown cntNumericUpDown;
+        private System.Windows.Forms.Button parseBtn;
+        private System.Windows.Forms.Label parsedCntLabel;
+        private System.Windows.Forms.Button downloadImgBtn;
+        private System.Windows.Forms.Label downloadedCntLabel;
+        private System.Windows.Forms.Label parsedInfo;
+    }
+}
+
---
+++ VKWallImagesParser/MainForm.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace VKWallImagesParser
+{
+    public partial class MainForm : Form
+    {
+        private List<Tuple<string, string>> _links;
+        private const string ApiPage = "https://api.vk.com/method/";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBtn_Click(object sender, EventArgs e)
+        {
+            if (groupNameTxtBox.Text != "")
+            {
+                var wallPageName = groupNameTxtBox.Text;
+                var pCount = decimal.Parse(GetWallPostsCount(wallPageName).ToString());
+                cntNumericUpDown.Value = cntNumericUpDown.Maximum = pCount;
+            }
+            else MessageBox.Show("Заполните поле!");
+
+        }
+
+        private void parseBtn_Click(object sender, EventArgs e)
+        {
+            var worker = new BackgroundWorker();
+            worker.DoWork += Parse_DoWork;
+            worker.RunWorkerCompleted += Parse_RunWorkerCompleted;
+            worker.RunWorkerAsync(new Tuple<string, int>(groupNameTxtBox.Text, int.Parse(cntNumericUpDown.Text)));
+        }
+
+        private void Parse_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var pp = (Tuple<string, int>) e.Argument;
+            var links = GetWallPostsImagesLinks(pp.Item1, pp.Item2);
+            e.Result = links;
+        }
+
+        private void Parse_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var res = (List<Tuple<string, string>>) e.Result;
+            _links = new List<Tuple<string, string>>();
+            _links = res;
+            downloadImgBtn.Enabled = true;
+        }
+
+        private int GetWallPostsCount(string wallPageName)
+        {
+            var https = new Https();
+            var count = 0;
+            const string method = "wall.get";
+            var vkParams = new NameValueCollection
+            {
+                {"v", "5.53"},
+                {"domain", wallPageName}
+            };
+            var response = https.POST(ApiPage + method, new CookieContainer(), vkParams);
+            try
+            {
+                count = int.Parse(JObject.Parse(response.Content)["response"]["count"].ToString());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неправильный адрес стены / страница не общедоступна");
+                //throw;
+            }
+            
+
+            return count;
+        }
+
+        private List<Tuple<string, string>> GetWallPostsImagesLinks(string wallPageName, int quantity)
+        {
+            var https = new Https();
+            var links = new List<Tuple<string, string>>();
+            
+            const string method = "wall.get";
+            var proceedCount = 0;
+            while (proceedCount < quantity)
+            {
+                var curCount = quantity - proceedCount;
+                if (curCount > 100) curCount = 100;
+                //else curCount = quantity - proceedCount;
+                var vkParams = new NameValueCollection
+                {
+                    {"v", "5.53"},
+                    {"domain", wallPageName},
+                    {"count", curCount.ToString()},
+                    {"offset", proceedCount.ToString()}
+                };
+                var response = https.POST(ApiPage + method, new CookieContainer(), vkParams);
+                
+                foreach (var jo in JObject.Parse(response.Content)["response"]["items"])
+                {
+                    if (jo["attachments"] == null) continue;
+                    var imgName = "wallid" + jo["id"];
+                    var tImageName = imgName;
+                    var jj = jo["attachments"] ?? jo["copy_history"]["attachments"];
+                    var attCnt = 1;
+                    foreach (var jo1 in jj)
+                    {
+                        imgName += "_" + attCnt++;
+                        if (!jo1["type"].ToString().Equals("photo")) continue;
+                        if (jo1["photo"]["photo_2560"] != null)
+                            links.Add(new Tuple<string, string>(imgName, jo1["photo"]["photo_2560"].ToString()));
+                        else if (jo1["photo"]["photo_1280"] != null)
+                            links.Add(new Tuple<string, string>(imgName, jo1["photo"]["photo_1280"].ToString()));
+                        else if (jo1["photo"]["photo_807"] != null)
+                            links.Add(new Tuple<string, string>(imgName, jo1["photo"]["photo_807"].ToString()));
+                        else if (jo1["photo"]["photo_604"] != null)
+                            links.Add(new Tuple<string, string>(imgName, jo1["photo"]["photo_604"].ToString()));
+                        else links.Add(new Tuple<string, string>(imgName, jo1["photo"]["photo_130"].ToString()));
+                        imgName = tImageName;
+                    }
+                }
+
+                proceedCount += curCount;
+                parsedCntLabel.Invoke(() => { parsedCntLabel.Text = "Спаршено: " + proceedCount + " из " + quantity; });
+                parsedInfo.Invoke(() => { parsedInfo.Text = "(найдено " + links.Count + " изображений)"; });
+            }
+            return links;
+        }
+
+        private void downloadImgBtn_Click(object sender, EventArgs e)
+        {
+            var FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() != DialogResult.OK) return;
+            downloadImgBtn.Enabled = false;
+            parseBtn.Enabled = false;
+            checkBtn.Enabled = false;
+            var worker = new BackgroundWorker();
+            worker.DoWork += Download_DoWork;
+            worker.RunWorkerCompleted += Download_RunWorkerCompleted;
+            worker.RunWorkerAsync(FBD.SelectedPath);
+        }
+
+        private void Download_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var folder = (string) e.Argument;
+            var cnt = 0;
+            var errCnt = 0;
+            foreach (var imgLink in _links)
+            {
+                var localFilename = folder + "\\" + imgLink.Item1 + ".jpg";
+                cnt++;
+                var isError = false;
+                using (var client = new WebClient())
+                {
+                    try
+                    {
+                        client.DownloadFile(imgLink.Item2, localFilename);
+                    }
+                    catch (WebException)
+                    {
+                        //MessageBox.Show(imgLink + " не рабочая");
+                        isError = true;
+                        errCnt++;
+                    } 
+                }
+
+                downloadedCntLabel.Invoke(
+                    () =>
+                    {
+                        downloadedCntLabel.Text = "Скачано: " + cnt + " из " + _links.Count;
+                        if (errCnt > 0) downloadedCntLabel.Text += " (" + errCnt + " ошибок)";
+                    });
+                if (isError) continue;
+            }
+            e.Result = folder;
+        }
+
+        private void Download_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var res = (string) e.Result;
+            parseBtn.Enabled = true;
+            downloadImgBtn.Enabled = true;
+            checkBtn.Enabled = true;
+            Process.Start(res);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public static class ControlExtensions
+    {
+        public static void Invoke(this Control control, Action action)
+        {
+            if (control.InvokeRequired) control.Invoke(new MethodInvoker(action), null);
+            else action.Invoke();
+        }
+    }
+}
